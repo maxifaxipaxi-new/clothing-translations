eede56374e8b703c3af7e4bf37c7ecaa794d5c63
Lloyd <70656116+1Blaze@users.noreply.github.com>
2023-08-14 22:23:04 +0100

    Create en.lua

---
+++ en.lua
@@ -0,0 +1,224 @@
+Language['en'] = {
+    nui = {
+        popouts = {
+            eyeColour = 'Eye Colour',
+            createOutfit = 'Create an Outfit',
+            outfit = 'Outfit',
+            deletePopup = {
+                title = 'Discard your current changes',
+                message = 'If you discard, this will clear all your changes',
+            },
+            deleteOutfitPopup = {
+                title = 'Delete your outfit',
+                message = 'If you discard, this will delete your outfit permanently',
+            },
+            infoPopup = {
+                title = 'Info',
+                message1 = 'Use the scroll wheel on your mouse to zoom in or out',
+                message2 = 'Click on the green disk icon to save your character',
+                message3 = 'Drag and hold the mouse to rotate the character',
+                message4 = 'Outfits can be saved by clicking on the hanger icon',
+            },
+            savePopup = {
+                title = 'Save your current changes',
+                price = 'Total Price: $',
+                message = 'If you don\'t save your changes will be lost',
+                bank = 'Bank',
+                cash = 'Cash',
+            },
+            tattooPopup = {
+                title = 'Clear your tattoos',
+                message = 'If you discard, this will remove all tattoos',
+            },
+            randomiserPopup = {
+                title = 'Randomise your character',
+                message = 'If you randomise, this will change your entire facial structure',
+            },
+            acceptOutfitPopup = {
+                title = 'Accept the outfit',
+                message = 'If you want to accept this outfit press accept otherwise press cancel',
+                message2 = 'Name:',
+            },
+            shareOutfitPopup = {
+                title = 'Share your outfit',
+                message = 'Player needs to be in the outfit menu',
+                noPlayers = 'No players nearby',
+            },
+            shareOutfitNotfiy = {
+                title = 'Successfully shared your outfit',
+                message = 'You have shared your outfit with %s',
+            }
+        },
+        menus = {
+            clothingNames = {
+                undershirt = 'Undershirt',
+                jacket = 'Jacket',
+                arms = 'Arms',
+                pants = 'Pants',
+                shoes = 'Shoes',
+                masks = 'Masks',
+                vests = 'Vests',
+                chains = 'Chains',
+                helmets = 'Helmets',
+                glasses = 'Glasses',
+                watches = 'Watches',
+                bracelets = 'Bracelets',
+                bags = 'Bags',
+                decals = 'Decals',
+                earrings = 'Earrings',
+            },
+            createCharacter = {
+                general = {
+                    boneDepth = 'Bone Depth',
+                    boneHeight = 'Bone Height',
+                    boneWidth = 'Bone Width',
+                    boneLength = 'Bone Length',
+                    boneShape = 'Bone Shape',
+                    boneHole = 'Bone Hole',
+                    boneCurve = 'Bone Curveness',
+                    boneTwist = 'Bone Twist',
+                    depth = 'Depth',
+                    height = 'Height',
+                    opening = 'Opening',
+                    thickness = 'Thickness',
+                    width = 'Width',
+                    peak = 'Peak',
+                    length = 'Length',
+                    tip = 'Tip',
+                },
+                chinTitle = 'Chin',
+                cheekTitle = 'Cheek',
+                eyebrowTitle = 'Eyebrow',
+                eyeTitle = 'Eyes',
+                jawTitle = 'Jaw',
+                lipTitle = 'Lip',
+                neckTitle = 'Neck',
+                noseTitle = 'Nose',
+                parents = {
+                    title = 'Parents',
+                    mother = 'Mother',
+                    father = 'Father',
+                }
+            },
+            peds = {
+                malePeds = 'Male Peds',
+                femalePeds = 'Female Peds',
+                gangMalePeds = 'Gang Male Peds',
+                gangFemalePeds = 'Gang Female Peds',
+                animalPeds = 'Animal Peds',
+                customPed = 'Custom Ped',
+                customPedPlaceholder = 'Custom Ped (Press Enter to Save)'
+            },
+            hairNames = {
+                hair = 'Hair',
+                beard = 'Beard',
+                eyebrows = 'Eyebrows',
+                makeup = 'Makeup',
+                blemishes = 'Blemishes',
+                age = 'Ageing',
+                blush = 'Blush',
+                chest = 'Chest Hair',
+                lipstick = 'Lipstick',
+                freckles = 'Moles/Freckles',
+                complextion = 'Complexion',
+                sundamage = 'Sun Damage',
+            },
+            outfits = {
+                yourOutfits = 'Your Outfits',
+                preSet = 'Pre Set Outfits',
+            },
+            tattoos = 'Tattoos'
+
+
+        },
+        general = {
+            save = 'Save',
+            delete = 'Delete',
+            discard = 'Discard',
+            cancel = 'Cancel',
+            name = 'Name',
+            close = 'Close',
+            clear = 'Clear',
+            opacity = 'Opacity',
+            models = 'Models',
+            variations = 'Variations',
+            randomise = 'Randomise',
+        },
+    },
+    cancelled = {
+        title = 'Cancelled Customisation',
+        message = 'You have cancelled, changes will not be saved',
+    },
+    outfits = {
+        savedOutfit = {
+            title = 'Saved Outfit',
+            message = 'You have saved your outfit name: %s',
+        },
+        usedOutfit = {
+            title = 'Used Outfit',
+            message = 'You have used your outfit name: %s',
+        },
+        deleteOutfit = {
+            title = 'Deleted Outfit',
+            message = 'You have deleted your outfit name: %s',
+        },
+        acceptOutfit = {
+            title = 'Accept Outfit',
+            message = 'You have accepted an outfit with the name: %s',
+        },
+        updateOutfitName = {
+            title = 'Updated Outfit Name',
+            message = 'You have updated the outfit name to: %s',
+        },
+        recievedOutfit = {
+            title = 'Recieved Outfit',
+            message = 'You have recieved an outfit with the name: %s',
+        }
+    },
+    saved = {
+        title = 'Saved Customisation',
+        message = 'You have saved your customisation',
+    },
+    payments = {
+        successful = {
+            title = 'Successful Payment - Saved Customisation',
+            message = 'You have successfully paid for your customisation and it has been saved!',
+        },
+        failure = {
+            title = 'Failed Payment',
+            message = 'You have failed to pay for your customisation - not enough money!',
+        }
+    },
+    -- clothing = {
+    --     peds = {
+    --         title = 'Applied Ped',
+    --         message = 'You have applied the ped: %s',
+    --     }
+    -- },
+    commands = {
+        reloadskin = {
+            help = 'Reloads the skin of the player',
+            succeeded = 'You have reloaded your skin',
+            failed = 'You have failed to reload your skin',
+            cooldown = 'You are on cooldown, please wait',
+        },
+        setped = {
+            help = 'Sets the ped of the player',
+            title = 'Successful ped set',
+            succeeded = 'You have set players ped to: %s',
+            mysuccess = 'Your ped has been set to: %s, go to a clothes store to save it',
+        }
+    },
+    showTextUI = {
+        clothing = 'Clothing Store',
+        tattoo = 'Tattoo Store',
+        surgeon = 'Plastic Surgery',
+        barber = 'Barber',
+        jobBasedClothing = 'Clothing Room',
+        personal = 'Outfits',
+        blacklist = {
+            title = 'Blacklisted Item',
+            message = 'You are using a blacklisted item! %s - not applying this item',
+        }
+    }
+}
