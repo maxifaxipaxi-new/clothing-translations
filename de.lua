6529b0b4086af2a6761eb8a001fc55ca29e8a670
MaxiFaxiPaxi <115405418+maxifaxipaxi-new@users.noreply.github.com>
2025-03-18 12:41:09 +0100

    Create de.lua

---
+++ de.lua
@@ -0,0 +1,163 @@
+-- german translation
+-- translated by mfpscripts.com
+
+Language['de'] = {
+    nui = {
+        popouts = {
+            eyeColour = 'Augenfarbe',
+            createOutfit = 'Outfit erstellen',
+            outfit = 'Outfit',
+            deletePopup = {
+                title = 'Aktuelle Änderungen verwerfen',
+                message = 'Wenn du verwirfst, werden alle Änderungen gelöscht',
+            },
+            deleteOutfitPopup = {
+                title = 'Outfit löschen',
+                message = 'Wenn du verwirfst, wird dein Outfit dauerhaft gelöscht',
+            },
+            infoPopup = {
+                title = 'Info',
+                message1 = 'Nutze das Mausrad, um hinein- oder herauszuzoomen',
+                message2 = 'Klicke auf das grüne Disketten-Symbol, um deinen Charakter zu speichern',
+                message3 = 'Ziehe und halte die Maus, um den Charakter zu drehen',
+                message4 = 'Outfits können gespeichert werden, indem du auf das Kleiderbügel-Symbol klickst',
+            },
+            savePopup = {
+                title = 'Änderungen speichern',
+                price = 'Gesamtpreis: $',
+                message = 'Wenn du nicht speicherst, gehen alle Änderungen verloren',
+                bank = 'Bank',
+                cash = 'Bargeld',
+            },
+            tattooPopup = {
+                title = 'Tattoos entfernen',
+                message = 'Wenn du verwirfst, werden alle Tattoos entfernt',
+            },
+            randomiserPopup = {
+                title = 'Charakter zufällig erstellen',
+                message = 'Wenn du zufällig erstellst, wird deine gesamte Gesichtsstruktur geändert',
+            },
+            acceptOutfitPopup = {
+                title = 'Outfit akzeptieren',
+                message = 'Wenn du dieses Outfit akzeptieren möchtest, drücke „Akzeptieren“, ansonsten „Abbrechen“',
+                message2 = 'Name:',
+            },
+            shareOutfitPopup = {
+                title = 'Outfit teilen',
+                message = 'Der Spieler muss sich im Outfit-Menü befinden',
+                noPlayers = 'Keine Spieler in der Nähe',
+            },
+            shareOutfitNotfiy = {
+                title = 'Outfit erfolgreich geteilt',
+                message = 'Du hast dein Outfit mit %s geteilt',
+            }
+        },
+        menus = {
+            clothingNames = {
+                undershirt = 'Unterhemd',
+                jacket = 'Jacke',
+                arms = 'Arme',
+                pants = 'Hose',
+                shoes = 'Schuhe',
+                masks = 'Masken',
+                vests = 'Westen',
+                chains = 'Ketten',
+                helmets = 'Helme',
+                glasses = 'Brillen',
+                watches = 'Uhren',
+                bracelets = 'Armbänder',
+                bags = 'Taschen',
+                decals = 'Aufkleber',
+                earrings = 'Ohrringe',
+            },
+            createCharacter = {
+                general = {
+                    boneDepth = 'Knochentiefe',
+                    boneHeight = 'Knochenhöhe',
+                    boneWidth = 'Knochenbreite',
+                    boneLength = 'Knochenlänge',
+                    boneShape = 'Knochenform',
+                    boneHole = 'Knochenloch',
+                    boneCurve = 'Knochenkrümmung',
+                    boneTwist = 'Knochenverdrehung',
+                    depth = 'Tiefe',
+                    height = 'Höhe',
+                    opening = 'Öffnung',
+                    thickness = 'Dicke',
+                    width = 'Breite',
+                    peak = 'Spitze',
+                    length = 'Länge',
+                    tip = 'Spitze',
+                },
+                chinTitle = 'Kinn',
+                cheekTitle = 'Wange',
+                eyebrowTitle = 'Augenbraue',
+                eyeTitle = 'Augen',
+                jawTitle = 'Kiefer',
+                lipTitle = 'Lippen',
+                neckTitle = 'Hals',
+                noseTitle = 'Nase',
+                parents = {
+                    title = 'Eltern',
+                    mother = 'Mutter',
+                    father = 'Vater',
+                }
+            },
+            peds = {
+                malePeds = 'Männliche NPCs',
+                femalePeds = 'Weibliche NPCs',
+                gangMalePeds = 'Gang-Männliche NPCs',
+                gangFemalePeds = 'Gang-Weibliche NPCs',
+                animalPeds = 'Tier-NPCs',
+                customPed = 'Benutzerdefinierter NPC',
+                customPedPlaceholder = 'Benutzerdefinierter NPC (Drücke Enter zum Speichern)'
+            },
+            hairNames = {
+                hair = 'Haare',
+                beard = 'Bart',
+                eyebrows = 'Augenbrauen',
+                makeup = 'Make-up',
+                blemishes = 'Hautunreinheiten',
+                age = 'Alterung',
+                blush = 'Rouge',
+                chest = 'Brusthaare',
+                lipstick = 'Lippenstift',
+                freckles = 'Muttermale/Sommersprossen',
+                complextion = 'Teint',
+                sundamage = 'Sonnenschäden',
+            },
+            outfits = {
+                yourOutfits = 'Deine Outfits',
+                preSet = 'Voreingestellte Outfits',
+            },
+            tattoos = 'Tattoos'
+        },
+        general = {
+            save = 'Speichern',
+            delete = 'Löschen',
+            discard = 'Verwerfen',
+            cancel = 'Abbrechen',
+            name = 'Name',
+            close = 'Schließen',
+            clear = 'Löschen',
+            opacity = 'Deckkraft',
+            models = 'Modelle',
+            variations = 'Variationen',
+            randomise = 'Zufällig',
+        },
+    },
+    cancelled = {
+        title = 'Anpassung abgebrochen',
+        message = 'Du hast abgebrochen, Änderungen wurden nicht gespeichert',
+    },
+    payments = {
+        successful = {
+            title = 'Erfolgreiche Zahlung',
+            message = 'Du hast erfolgreich für deine Anpassung bezahlt und sie wurde gespeichert!',
+        },
+        failure = {
+            title = 'Zahlung fehlgeschlagen',
+            message = 'Du konntest nicht bezahlen – nicht genug Geld!',
+        }
+    }
+}
